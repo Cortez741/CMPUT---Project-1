89912dee1ee2bbc25703d5f4cc6abd7157e0fd55
Victor Frunza <victor.f@outlook.com>
2016-03-04 12:31:06 -0700

    Added Interface

--- NVR.py
+++ NVR.py
@@ -3,9 +3,18 @@
 # Instructor: Li-Yan Yuan
 # TA: Kriti Khare
 # Filename: NVR.py
-#----------------------------------------------------------------------------------------------------------------------#
+
+#---Description--------------------------------------------------------------------------------------------------------#
 # Description: This component is used to register a new vehicle by an auto registration officer. By a new vehicle, we
 # mean a vehicle that has not been registered in the database. The component shall allow an officer to enter the
 # detailed information about the vehicle and personal information about its new owners, if it is not in the database.
-# You may assume that all the information about vehicle types has been loaded in the initial database. 
-#----------------------------------------------------------------------------------------------------------------------#
+# You may assume that all the information about vehicle types has been loaded in the initial database.
+
+#---Interface----------------------------------------------------------------------------------------------------------#
+# Input: serial_no, maker, model, year, color, type_id, owner_id, is_primary_owner
+# Ouput: CSV text (STR) to send back to the front end.
+
+#---Function Start-----------------------------------------------------------------------------------------------------#
+def NVR(serial_no, maker, model, year, color, type_id, owner_id, is_primary_owner):
+
+    vehicle_SQL = 'INSERT INTO vehicle VALUES ('+serial_no+', '+maker+', '+model+', '+year+', '+color+', '+type_id+');'
