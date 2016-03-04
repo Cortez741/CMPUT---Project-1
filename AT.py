8cc82583bdcd6f8fecca5ca84df54318a79a9f87
Victor Frunza <victor.f@outlook.com>
2016-03-03 23:31:10 -0700

    Created Victor's two modules for the first two components

---
+++ AT.py
@@ -0,0 +1,10 @@
+#---Auto Transaction---------------------------------------------------------------------------------------------------#
+# Authors: Victor Frunza & Ian Oltuszyk
+# Instructor: Li-Yan Yuan
+# TA: Kriti Khare
+# Filename: NVR.py
+#----------------------------------------------------------------------------------------------------------------------#
+# Description: This component is used to complete an auto transaction. Your program shall allow the officer to enter all
+# necessary information to complete this task, including, but not limiting to, the details about the seller, the buyer,
+# the date, and the price. The component shall also remove the relevant information of the previous ownership.
+#----------------------------------------------------------------------------------------------------------------------#
---
+++ NVR.py
@@ -0,0 +1,11 @@
+#---New Vehicle Registration-------------------------------------------------------------------------------------------#
+# Authors: Victor Frunza & Ian Oltuszyk
+# Instructor: Li-Yan Yuan
+# TA: Kriti Khare
+# Filename: NVR.py
+#----------------------------------------------------------------------------------------------------------------------#
+# Description: This component is used to register a new vehicle by an auto registration officer. By a new vehicle, we
+# mean a vehicle that has not been registered in the database. The component shall allow an officer to enter the
+# detailed information about the vehicle and personal information about its new owners, if it is not in the database.
+# You may assume that all the information about vehicle types has been loaded in the initial database. 
+#----------------------------------------------------------------------------------------------------------------------#
